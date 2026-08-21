af67666705e37a5132f4372188b333c3a4ca978e
hanjinghong626-ctrl <hanjinghong626@gmail.com>
2026-08-21 16:17:43 +0300

    test: minimal page to verify site functionality

---
+++ frontend/app/test/page.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{padding: '100px', textAlign: 'center'}}>
+      <h1>Test Page - Site is working!</h1>
+      <p>If you see this, the Next.js app is functional.</p>
+    </div>
+  )
+}
